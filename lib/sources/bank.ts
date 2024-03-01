0525da8fecd46c3a941d404cb99328b6b705a4f8
Matheus Torres <torresmiranda.rj@gmail.com>
2024-03-01 11:17:23 -0300

    refactor: rename method

--- lib/sources/bank.ts
+++ lib/sources/bank.ts
@@ -5,11 +5,11 @@ import { type Bank } from '../typings';
 import { type BrasilAPIResponse, type Result } from '../typings/result';
 
 interface IBank {
-	getOne(code: string): Promise<Result<Bank>>;
+	fetch(code: string): Promise<Result<Bank>>;
 }
 
 export class BrasilAPIBank extends Source implements IBank {
-	async getOne(code: string): Promise<Result<Bank>> {
+	async fetch(code: string): Promise<Result<Bank>> {
 		const res = await HttpsClient.GET<BrasilAPIResponse<Bank>>(
 			`https://brasilapi.com.br/api/banks/v1/${code}`,
 		);
