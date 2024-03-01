6fd131cc93d051c7b7cb826ada46982e5fd9b96f
Matheus Torres <torresmiranda.rj@gmail.com>
2024-03-01 15:59:54 -0300

    test: ensure SUT lists all banks

--- tests/brasilapi.spec.ts
+++ tests/brasilapi.spec.ts
@@ -3,6 +3,15 @@ import { beforeEach, describe, it } from 'node:test';
 
 import { BrasilAPI } from '@/lib/brasilapi';
 
+const objHasKeys = <T extends object>(
+	obj: T,
+	expectedKeys: string[],
+): boolean => {
+	const objKeys = Object.keys(obj);
+
+	return expectedKeys.every((k) => objKeys.includes(k));
+};
+
 describe('BrasilAPI', () => {
 	let sut: BrasilAPI;
 
@@ -24,5 +33,22 @@ describe('BrasilAPI', () => {
 					'ICAP do Brasil Corretora de Títulos e Valores Mobiliários Ltda.',
 			},
 		});
+	});
+
+	it('should list all banks', async () => {
+		const res = await sut.bank.list({
+			limit: 10,
+			take: 5,
+		});
+
+		assert.ok(res);
+		assert.equal(res.data!.length, 5);
+
+		for (const item of res.data!) {
+			assert.deepStrictEqual(
+				objHasKeys(item, ['ispb', 'name', 'code', 'fullName']),
+				true,
+			);
+		}
 	});
 });
