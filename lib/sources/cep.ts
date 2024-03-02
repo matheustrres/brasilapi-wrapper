2bbadc1379fa31b6fbd8cb9b6cf2df1b7c1b24c4
Matheus Torres <torresmiranda.rj@gmail.com>
2024-03-02 12:38:03 -0300

    feat: impl fetchV2 method

--- lib/sources/cep.ts
+++ lib/sources/cep.ts
@@ -6,6 +6,7 @@ import { type BrasilAPIResponse, type Result } from '../typings/result';
 
 interface ICEP {
 	fetchV1(cep: string): Promise<Result<CEP>>;
+	fetchV2(cep: string): Promise<Result<CEP>>;
 }
 
 export class BrasilAPICEP extends Source implements ICEP {
@@ -14,6 +15,14 @@ export class BrasilAPICEP extends Source implements ICEP {
 	async fetchV1(cep: string) {
 		const res = await HttpsClient.GET<BrasilAPIResponse<CEP>>(
 			`${BrasilAPICEP.#URL}/v1/${cep}`,
+		);
+
+		return this.followUp<CEP>(res);
+	}
+
+	async fetchV2(cep: string) {
+		const res = await HttpsClient.GET<BrasilAPIResponse<CEP>>(
+			`${BrasilAPICEP.#URL}/v2/${cep}`,
 		);
 
 		return this.followUp<CEP>(res);
